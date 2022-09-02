71eb95cbb59cc7ac3eb821496297a7e3144bfc64
Simão sá <simaosa@Simaos-MacBook-Pro.local>
2022-09-02 18:05:35 +0100

    Fixed pull request code

---
+++ pull_request.py
@@ -0,0 +1,26 @@
+import requests
+import git
+import json
+def create_pull_request(project_name, repo_name, title, description, head_branch, base_branch, git_token):
+    """Creates the pull request for the head_branch against the base_branch"""
+    git_pulls_api = "https://api.github.com/repos/{0}/{1}/pulls".format(
+        project_name,
+        repo_name)
+    headers = {
+        "Authorization": "token {0}".format(git_token),
+        "Content-Type": "application/vnd.github+json"}
+
+    payload = {
+        "title": title,
+        "body": description,
+        "head": head_branch,
+        "base": base_branch,
+    }
+
+    r = requests.post(
+        git_pulls_api,
+        headers=headers,
+        data=json.dumps(payload))
+
+    if not r.ok:
+        print("Request Failed: {0}".format(r.text))
